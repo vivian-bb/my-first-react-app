e59f461e174d14817797b24aeaafc717a65d844e
vivian-bb <73925845+vivian-bb@users.noreply.github.com>
2022-11-17 23:17:16 +1100

    create first component and create style.js

---
+++ src/containers/home/index.js
@@ -0,0 +1,24 @@
+//import React, { Component }from "react";
+//class component
+// class Home extends Component {
+//     render() {
+//         return (
+
+//         )
+//     }
+
+// }
+
+//function component + hookComponent more useful now
+
+function Home() {
+    return (
+        <div>
+            <p>Home</p>
+        </div>
+    )
+}
+
+
+//must be export after import in app.js 
+export default Home;
